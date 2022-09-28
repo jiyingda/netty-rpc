3983c55bd7cb77f11b5e1555eacab22e1c86a23c
jiyingdabj <jiyingdabj@yuanfudao.com>
2022-09-28 19:20:38 +0800

    ADD: RpcService

---
+++ netty-rpc-service/src/main/java/com/cpf/nettyrpc/service/RpcService.java
@@ -0,0 +1,13 @@
+/**
+ * @(#)RpcService.java, 9月 28, 2022.
+ * <p>
+ * Copyright 2022 yuanfudao.com. All rights reserved.
+ * FENBI.COM PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com.cpf.nettyrpc.service;
+
+/**
+ * @author jiyingdabj
+ */
+public class RpcService {
+}
